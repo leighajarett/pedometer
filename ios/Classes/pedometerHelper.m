201d6b6f9a7bf652b55be3e19eb142d3a9d9862f
Brian Quinlan <bquinlan@google.com>
2022-12-13 11:49:53 -0800

    Make it build!

--- ios/Classes/pedometerHelper.m
+++ ios/Classes/pedometerHelper.m
@@ -1,3 +1,3 @@
 // Relative import to be able to reuse the C sources.
 // See the comment in ../{projectName}}.podspec for more information.
- #include "../../src/pedometerHelper.h"
+ #include "../../src/pedometerHelper.m"
